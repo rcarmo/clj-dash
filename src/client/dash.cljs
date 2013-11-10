f983b41508c79a29f0217ca1aaa0198f4a3217f4
Rui Carmo <rui.carmo@gmail.com>
2013-11-10 01:17:10 +0000

    started merging charts

--- src/client/dash.cljs
+++ src/client/dash.cljs
@@ -6,10 +6,16 @@
             clang.directive.clangRepeat)
   (:use [clang.util :only [? module]]))
 
-(def m (module "clj-dash" ["clang"]))
+(def m (module "clj-dash" ["clang" "angles"]))
 
 (def.controller m chart [$scope]
-  (assoc! $scope :data (into [] (map (fn [%] {:x % :y (/ % 10)}) (range 0 10))))
-
+  (assoc! $scope :data {:labels   (into [] (range 0 20))
+                        :datasets [{:data             (into [] (range 0 20))
+                                    :fillColor        "rgba(151,187,205,0)"
+                                    :strokeColor      "#e67e22"
+                                    :pointColor       "rgba(151,187,205,0)"
+                                    :pointStrokeColor "#e67e22"}]})
+  (assoc! $scope :options {:segmentStrokeColor "#fff"})
+  
   (defn.scope count []
 	(count (:data $scope))))
